ec0afcb4c3ab14ad3a0dde034e6689102264a957
talhakhalil0703 <tkhalil0703@gmail.com>
2022-09-29 00:07:21 -0600

    Multiple runs in tester

--- tester.py
+++ tester.py
@@ -8,6 +8,7 @@ CONVERGENCE_THRESHOLD = 0.000001
 SEED = 8675309
 GRADER_THRESHOLD = 0.0000100000001 # floating point errors
 EXEC = "./bin/kmeans"
+RUNS = 5
 
 METHODS = ["--use_cuda_basic"]
 
@@ -20,32 +21,34 @@ def main():
     # This test file is called by make, so it does not need to make...
     for method in METHODS:
         for input,dims,answer in zip(INPUT_FILES, DIMS,  INPUT_ANSWERS_FILES):
-            command_to_run = f"{EXEC} -k 16 -d {dims} -i {input} -m {ITERATION_LIMIT} -t {CONVERGENCE_THRESHOLD} -s {SEED} {method}"
-            p = subprocess.run([command_to_run], shell=True, capture_output=True, text=True)
-            match = True
-            cluster = []
-            with open(f"{answer}", "r") as f:
-                for point in f.readlines():
-                    point = point.replace('\n', '')
-                    dim = []
-                    for id, dimension in enumerate(point.split(' ')):
-                        if id == 0: continue
-                        dim.append(float(dimension))
-                    cluster.append(dim)
+            timing = 0
+            for run in range(0, RUNS):
+                command_to_run = f"{EXEC} -k 16 -d {dims} -i {input} -m {ITERATION_LIMIT} -t {CONVERGENCE_THRESHOLD} -s {SEED} {method}"
+                p = subprocess.run([command_to_run], shell=True, capture_output=True, text=True)
+                match = True
+                cluster = []
+                with open(f"{answer}", "r") as f:
+                    for point in f.readlines():
+                        point = point.replace('\n', '')
+                        dim = []
+                        for id, dimension in enumerate(point.split(' ')):
+                            if id == 0: continue
+                            dim.append(float(dimension))
+                        cluster.append(dim)
 
-            for p_id, point in enumerate(p.stdout.splitlines()):
-                if p_id == 0:
-                    timing = point.split(',')[1]
-                    continue # first line is timing information
-                for d_id, dimension in enumerate(point.split(" ")): # First number is index, we can skip, subsequent DIMS numbers
-                    if d_id == 0 or d_id==dims+1: continue # Random space at end so we skip that as well
-                    if float(dimension) < (cluster[p_id-1][d_id-1]  - GRADER_THRESHOLD) or float(dimension) > (cluster[p_id-1][d_id-1]  + GRADER_THRESHOLD):
-                        print(f"Output: {float(dimension)}, Answer: {cluster[p_id-1][d_id-1] - GRADER_THRESHOLD} - {cluster[p_id-1][d_id-1] + GRADER_THRESHOLD}")
-                        match = False
+                for p_id, point in enumerate(p.stdout.splitlines()):
+                    if p_id == 0:
+                        timing += float(point.split(',')[1])
+                        continue # first line is timing information
+                    for d_id, dimension in enumerate(point.split(" ")): # First number is index, we can skip, subsequent DIMS numbers
+                        if d_id == 0 or d_id==dims+1: continue # Random space at end so we skip that as well
+                        if float(dimension) < (cluster[p_id-1][d_id-1]  - GRADER_THRESHOLD) or float(dimension) > (cluster[p_id-1][d_id-1]  + GRADER_THRESHOLD):
+                            print(f"Output: {float(dimension)}, Answer: {cluster[p_id-1][d_id-1] - GRADER_THRESHOLD} - {cluster[p_id-1][d_id-1] + GRADER_THRESHOLD}")
+                            match = False
             if not match:
                 print(f"{input} does not match for {method}")
             else:
-                print(f"{command_to_run} : {timing} ms")
+                print(f"{command_to_run} : {timing/RUNS} ms, averaged over {RUNS} runs")
 
 
 if __name__ == "__main__":

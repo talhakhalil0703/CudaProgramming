3a3d9c65eab3dd5b6e8f36c515c8da81e67c3cd5
talhakhalil0703 <tkhalil0703@gmail.com>
2022-09-28 22:14:27 -0600

    Automatic tester

---
+++ tester.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import subprocess
+from sys import stdout
+from unittest import TextTestResult
+
+ITERATION_LIMIT = 150
+CONVERGENCE_THRESHOLD = 0.000001
+SEED = 8675309
+GRADER_THRESHOLD = 0.0000100000001 # floating point errors
+EXEC = "./bin/kmeans_cuda "
+
+METHODS = ["--use_cuda_basic"]
+
+INPUT_ANSWERS_FILES = ["input/random-n2048-d16-c16-answer.txt", "input/random-n16384-d24-c16-answer.txt", "input/random-n65536-d32-c16-answer.txt"]
+INPUT_FILES = ["input/random-n2048-d16-c16.txt", "input/random-n16384-d24-c16.txt", "input/random-n65536-d32-c16.txt"]
+DIMS = [16, 24, 32]
+
+
+def main():
+    # This test file is called by make, so it does not need to make...
+    for method in METHODS:
+        for input,dims,answer in zip(INPUT_FILES, DIMS,  INPUT_ANSWERS_FILES):
+            command_to_run = f"{EXEC} -k 16 -d {dims} -i {input} -m {ITERATION_LIMIT} -t {CONVERGENCE_THRESHOLD} -s {SEED} {method}"
+            p = subprocess.run([command_to_run], shell=True, capture_output=True, text=True)
+            match = True
+            cluster = []
+            with open(f"{answer}", "r") as f:
+                for point in f.readlines():
+                    point = point.replace('\n', '')
+                    dim = []
+                    for id, dimension in enumerate(point.split(' ')):
+                        if id == 0: continue
+                        dim.append(float(dimension))
+                    cluster.append(dim)
+
+            for p_id, point in enumerate(p.stdout.splitlines()):
+                if p_id == 0:
+                    continue # first line is timing information
+                for d_id, dimension in enumerate(point.split(" ")): # First number is index, we can skip, subsequent DIMS numbers
+                    if d_id == 0 or d_id==dims+1: continue # Random space at end so we skip that as well
+                    if float(dimension) < (cluster[p_id-1][d_id-1]  - GRADER_THRESHOLD) or float(dimension) > (cluster[p_id-1][d_id-1]  + GRADER_THRESHOLD):
+                        print(f"Output: {float(dimension)}, Answer: {cluster[p_id-1][d_id-1] - GRADER_THRESHOLD} - {cluster[p_id-1][d_id-1] + GRADER_THRESHOLD}")
+                        match = False
+            if not match:
+                print(f"{input} does not match for {method}")
+
+
+if __name__ == "__main__":
+    main()
